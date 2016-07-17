5bf7bf1de9dae9931da26aff7f323a1b9633e199
Paul Hobson <pmhobson@gmail.com>
2016-07-17 13:52:34 -0700

    rebuild docs with new CIs

--- api/probscale-1.py
+++
@@ -1,5 +0,0 @@
-from matplotlib import pyplot
-import probscale
-fig, ax = pyplot.subplots(figsize=(4, 7))
-ax.set_ylim(bottom=0.5, top=99.5)
-ax.set_yscale('prob')
--- api/viz-2.py
+++
@@ -1,5 +0,0 @@
-fig = probplot(data, plottype='qq', probax='x',
-         problabel='Theoretical Quantiles',
-         datalabel='Observed values', bestfit=True,
-         line_kws=dict(linestyle='-', linewidth=2),
-         scatter_kws=dict(marker='s', alpha=0.5))
